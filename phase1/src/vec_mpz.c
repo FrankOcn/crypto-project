68ef28102a2e16da49a9bac807d54b04872a490b
Kai Fu Yung <kaifu.yung@gmail.com>
2016-11-21 17:08:12 -0500

    Create prototype for batch div

--- phase1/src/vec_mpz.c
+++ phase1/src/vec_mpz.c
@@ -1,6 +1,19 @@
 #include "vec_mpz.h"
 #include "stdlib.h"
+#include "math.h"
 #include "gmp.h"
+
+unsigned int log_2(int v)
+{
+  if (v == 1) return 0;
+  unsigned int ret = 0;
+  while (v)
+  {
+    v >>= 1;
+    ret++;
+  }
+  return ret;
+}
 
 struct vec_mpz_t* vec_mpz_init() {
   struct vec_mpz_t* vec = (struct vec_mpz_t*)malloc(sizeof(struct vec_mpz_t));
@@ -62,10 +75,84 @@ struct vec_mpz_t* vec_mpz_product_tree(struct vec_mpz_t* vec) {
   }
   for (i = product_tree->size - vec->size - 1; i >= 0; --i) {
     mpz_mul(
-      product_tree->elems[i],
-      product_tree->elems[i*2 + 1],
-      product_tree->elems[i*2 + 2]
-    );
+        product_tree->elems[i],
+        product_tree->elems[i*2 + 1],
+        product_tree->elems[i*2 + 2]
+        );
   }
   return product_tree;
+}
+
+// Given integer n, and sequence of integers X[]:
+// returns vec of [ (n mod X[0]), (n mod X[1]), (n mod X[2])..  ]
+struct vec_mpz_t* vec_mpz_remainder_tree(mpz_t n, struct vec_mpz_t* vec) 
+{
+  int i;
+  struct vec_mpz_t* remainder_tree = vec_mpz_init_size(vec->size);
+  
+  // gmp_printf("%Zd mod %Zd\n", n, vec->elems[0]);
+  mpz_mod(remainder_tree->elems[0], n, vec->elems[0]);
+  for (i = 1; i < remainder_tree->size; i++)
+  {
+    if (i % 2 == 0)
+    {
+      // gmp_printf("%Zd mod %Zd\n", remainder_tree->elems[i/2 -1], vec->elems[i]);
+      mpz_mod(remainder_tree->elems[i], remainder_tree->elems[i/2 - 1], vec->elems[i]);
+    }
+    else
+    {
+      // gmp_printf("%Zd mod %Zd\n", remainder_tree->elems[i/2], vec->elems[i]);
+      mpz_mod(remainder_tree->elems[i], remainder_tree->elems[i/2], vec->elems[i]);
+    }
+  }
+  return remainder_tree;
+}
+
+struct vec_mpz_t* remainders_in(struct vec_mpz_t* remainder_tree, int batch_size)
+{
+  struct vec_mpz_t* remainders = vec_mpz_init_size(batch_size);
+  int j = remainder_tree->size - 1;
+  for (int i = 0; i < batch_size; i++)
+  {
+    mpz_set(remainders->elems[i], remainder_tree->elems[j - i]);
+  }
+  return remainders;
+}
+
+// iterate e until 2^(2^e) > x_k
+// then push remainder[i]^(2^2)
+struct vec_mpz_t* compute_squares(struct vec_mpz_t* remainders, struct vec_mpz_t* batch)
+{
+  struct vec_mpz_t* squares = vec_mpz_init();
+  mpz_t temp, two_sq_sq;
+  mpz_init(temp);
+  mpz_init(two_sq_sq);
+  uint64_t two_sq;
+
+  for (int i = 0; i < remainders->size; i++)
+  {
+    two_sq = 1;
+    mpz_ui_pow_ui(two_sq_sq, 2, two_sq);
+    while (mpz_cmp(two_sq_sq, batch->elems[i]) < 0)
+    {
+      two_sq <<= 1;
+      mpz_ui_pow_ui(two_sq_sq, 2, two_sq);
+    }
+    mpz_powm_ui(temp, remainders->elems[i], two_sq, batch->elems[i]);
+    vec_mpz_insert(squares, temp);
+  }
+
+  return squares;
+}
+
+struct vec_mpz_t* compute_smooth_parts(struct vec_mpz_t* batch, struct vec_mpz_t* squares)
+{
+  mpz_t temp;
+  mpz_init(temp);
+  struct vec_mpz_t* smooth_parts = vec_mpz_init_size(batch->size);
+  for (int i = 0; i < batch->size; i++)
+  {
+    mpz_gcd(smooth_parts->elems[i], batch->elems[i], squares->elems[i]);
+  }
+  return smooth_parts;
 }
